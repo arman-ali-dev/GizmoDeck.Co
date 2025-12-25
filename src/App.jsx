d2e177457f11de9673cd5a50b1579778e016c422
arman5911 <armaanali.dev@gmail.com>
2025-12-25 17:04:06 +0530

    decrease loading time

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ const App = () => {
     const timer = setTimeout(() => {
       setLoading(false);
       document.body.style.overflow = "auto";
-    }, 4000);
+    }, 2000);
 
     return () => clearTimeout(timer);
   }, [location.pathname]);
